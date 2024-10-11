2a3bc2f422e727af1b60b63e7e89ec9e02c7e4f2
Guang Yang <114556848+web3nemo@users.noreply.github.com>
2024-10-11 17:48:38 +0800

    Fix all fileheader stylecops issues. - Add fileheader automatically with .editorconfig file - Add stylecop.json to define companyName and copyrightText.

--- src/Perkify.Core/IExpiry.cs
+++ src/Perkify.Core/IExpiry.cs
@@ -1,22 +1,27 @@
-﻿namespace Perkify.Core
+﻿// <copyright file="IExpiry.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Perkify.Core
 {
     /// <summary>Maintain the current time in UTC.</summary>
     public interface INowUtc
     {
-        /// <summary>The current time in UTC.</summary>
+        /// <summary>Gets the current time in UTC.</summary>
         public DateTime NowUtc { get; }
     }
 
     /// <summary>The interface to maintain an expiry time with grace period for eligibility.</summary>
-    public interface IExpiry<T> where T: IExpiry<T>, INowUtc
+    public interface IExpiry<T>
+        where T : IExpiry<T>, INowUtc
     {
         #region Remaining & Overdue
 
-        /// <summary>The Grace period as absolute time span.</summary>
+        /// <summary>Gets the Grace period as absolute time span.</summary>
         public TimeSpan GracePeriod { get; }
 
         /// <summary>
-        /// The remaining portion.
+        /// Gets the remaining portion.
         /// - If suspended, the remaining portion is the time between suspend time and expiry time.
         /// - If eligible, the remaining portion is the current time between now and expiry time.
         /// - If ineligible (after grace period), the remaining portion is negative grace period.
@@ -24,7 +29,7 @@
         public TimeSpan Remaining { get; }
 
         /// <summary>
-        /// The overdue portion.
+        /// Gets the overdue portion.
         /// - Suspended: The overdue portion is the time between suspend time and expiry time. Zero if suspend time is earlier than expiry time.
         /// - Eligible: The overdue portion is the time between expiry time and now. Zero if now is earlier than expiry time.
         /// - Ineligible (after grace period): The overdue portion is the grace period.
@@ -35,10 +40,10 @@
 
         #region Renew
 
-        /// <summary>The expiry time in UTC.</summary>
+        /// <summary>Gets the expiry time in UTC.</summary>
         public DateTime ExpiryUtc { get; }
 
-        /// <summary>The renewal period based on ISO8601 duration string and flag to identify calendar arithmetic.</summary>
+        /// <summary>Gets the renewal period based on ISO8601 duration string and flag to identify calendar arithmetic.</summary>
         public Renewal? Renewal { get; }
 
         /// <summary>Renew the expiry time in timeline arithmetic or calendrical arithmetic.</summary>
@@ -51,7 +56,7 @@
         #region Deactivate & Activate
 
         /// <summary>
-        /// The suspend time in UTC.
+        /// Gets the suspend time in UTC.
         /// - Explicit suspension time (smaller or equals to deadline time) if any suspension is applied.
         /// - Implicit suspension time (equals to deadline time) when the expiry time is ineligible.
         /// - Null when the expiry time is eligible (and no suspension is applied).
@@ -59,7 +64,7 @@
         public DateTime? SuspensionUtc { get; }
 
         /// <summary>
-        /// Boolean flag to identify if the expiry time is active.
+        /// Gets a value indicating whether boolean flag to identify if the expiry time is active.
         /// - True if the expiry time is deactivated (suspended).
         /// - False if the expiry time is activated (not suspended).
         /// </summary>
@@ -84,11 +89,15 @@
     public static class ExpiryExtensions
     {
         /// <summary>The deadline time in UTC.</summary>
-        public static DateTime GetDeadlineUtc<T>(this T expiry) where T: IExpiry<T>, INowUtc
+        /// <returns></returns>
+        public static DateTime GetDeadlineUtc<T>(this T expiry)
+            where T : IExpiry<T>, INowUtc
             => expiry.ExpiryUtc + expiry.GracePeriod;
 
         /// <summary>The boolean flag to identify if the expiry time has been reached.</summary>
-        public static bool IsExpired<T>(this T expiry) where T: IExpiry<T>, INowUtc
+        /// <returns></returns>
+        public static bool IsExpired<T>(this T expiry)
+            where T : IExpiry<T>, INowUtc
             => (expiry.SuspensionUtc ?? expiry.NowUtc) >= expiry.ExpiryUtc;
     }
 }
